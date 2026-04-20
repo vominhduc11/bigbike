36cf4e735ff889190478122613425d20921141ba
Your Name <your_email@example.com>
2026-04-20 09:46:18 +0700

    update

---
+++ bigbike-backend/src/main/java/com/bigbike/bigbike_backend/BigbikeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bigbike.bigbike_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigbikeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigbikeBackendApplication.class, args);
+	}
+
+}
